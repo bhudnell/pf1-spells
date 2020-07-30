09e1c9c972f75831c2f1ece9e541d1964b4e9fd0
bhudnell1240 <bthudnell@gmail.com>
2020-07-29 17:14:26 -0700

    moved query parameter handling to serverUtilities

--- server.js
+++ server.js
@@ -47,43 +47,18 @@ app.get('/api/allspells', (req, res) => {
 
 app.get('/api/spellsearch', (req, res) => {
   // get the query parameters
-  const queryObject = url.parse(req.url,true).query;
+  const queryObject = utils.processQuery(new URL(req.url, "https://dummyurl.com"));
 
   // create the query string
   let queryString = 'SELECT * FROM spells WHERE';
   let firstCondition = true;
 
+  // create the query string using the queryObject's parameters
   Object.entries(queryObject).forEach(entry => {
     console.log(entry);
 
-    if (entry[0] === 'searchString')
-    {
-      console.log(`search string is: ${entry[1]}`);
-
-      firstCondition = false;
-      queryString.concat(` ${entry[0]} LIKE '%${entry[1]}%'`); // probably shouldnt be entry[0], probably only spell name? or maybe description too?
-    }
-    else if (entry[1] === 'true') {
-      console.log(`parameter is accepted: ${entry[0]}`);
-
-      if (!firstCondition) {
-        firstCondition = false;
-        queryString.concat(` AND`);
-      }
-
-      queryString.concat(` ${entry[0]} = TRUE`); // todo check this condition
-    }
-    else {
-      console.log(`parameter is rejected: ${entry[0]}`);
-    }
+    // todo
   });
-
-  // queryObject should be an object of a list of params and values
-  // ie: { sorcerer: false, cleric: true, 0th: true, fortSave: true }
-  
-  // create a sql query to return the spells database, potentially
-  // modified with the params? (or maybe one api to access all, one
-  // to handle a search request)
 
   // create database connection
   const connection = mysql.createConnection({
@@ -107,12 +82,9 @@ app.get('/api/spellsearch', (req, res) => {
 
       // here we need to handle what to do with the result from the query
       console.log(result);
+      res.json(result);
     });
   });
-
-  // Return them as json? (idk if this is the best way or not)
-  // maybe goes in the above con.query
-  res.json(result);
 });
 
 // The "catchall" handler: for any request that doesn't
--- serverUtilities.js
+++ serverUtilities.js
@@ -2,11 +2,92 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 
-exports.createMySQLConnection = () => 
-    mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DBASE,
-        port: process.env.DB_PORT
-      })
+const classTypes = [
+  "alchemist" ,
+  "antipaladin",
+  "arcanist",
+  "bard",
+  "bloodrager",
+  "cleric",
+  "druid",
+  "hunter",
+  "inquisitor",
+  "investigator",
+  "magus",
+  "spell_medium",
+  "mesmerist",
+  "occultist",
+  "oracle",
+  "paladin",
+  "psychic",
+  "ranger",
+  "shaman",
+  "skald",
+  "sorc",
+  "spiritualist",
+  "summoner",
+  "summoner_unchained",
+  "witch",
+  "wiz"
+];
+
+const saveTypes = [
+  "fortitude",
+  "reflex",
+  "will",
+  "none"
+];
+
+const spellLevels = [
+  "0th",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "5th",
+  "6th",
+  "7th",
+  "8th",
+  "9th"
+]
+
+exports.createMySQLConnection = () => {
+  mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DBASE,
+    port: process.env.DB_PORT
+  })
+}
+
+exports.processQuery = url => {
+  const processObject = {
+    classes: [],
+    saves: [],
+    spellLevels: []
+  };
+
+  url.searchParams.forEach((value, key) => {  
+    if (key === 'searchString') { // text user is searching for
+      processObject.searchString = value;
+    }
+    else if (value === 'true') // only accept true values for now
+    {
+      if (key === 'spell_resistance') { // spell resistance toggle
+        processObject.spellResistance = value;
+      }
+      else if (classTypes.includes(key)) { // this is a class type parameter
+        processObject.classes.push(key);
+      }
+      else if (saveTypes.includes(key)) { // this is a save type parameter
+        processObject.saves.push(key);
+      }
+      else if (spellLevels.includes(key)) { // this is a spell level parameter
+        processObject.spellLevels.push(key);
+      }
+    }
+  });
+
+  return processObject;
+}

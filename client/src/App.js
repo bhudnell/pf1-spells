fcda7f2beb3dd66f3a81c056b0d7f2d575998b92
bhudnell1240 <bthudnell@gmail.com>
2020-08-22 14:16:51 -0700

    Added toggle to switch between light/dark themes

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,37 @@ import './App.scss';
 
 import { Main } from './Main';
 
+const changeTheme = () => {
+  if (document.documentElement.hasAttribute('theme')) {
+    document.documentElement.removeAttribute('theme');
+  }
+  else {
+    document.documentElement.setAttribute('theme', 'dark');
+  }
+}
+
 function App() {
   return (
     <div className="App">
-      <Main />
+      <div className="content">
+        <header>
+          <br />
+          <h1>Pathfinder 1E Spell Search</h1>
+          <input type="checkbox" id="themeSwitch" className="themeSwitchInput" onClick={changeTheme} />
+          <label htmlFor="themeSwitch" className="themeSwitchLabel">
+            <span />
+          </label>
+          <span className="themeLabel">Toggle theme</span>
+          <br />
+        </header>
+        <br />
+        <Main />
+      </div>
+      <footer>
+        <h6>
+          Database sourced from <a href="http://home.pathfindercommunity.net/home/databases/spells" target="_blank" rel="noopener noreferrer">pathfindercommunity.net</a>. Designed by Brendon Hudnell. 2020
+        </h6>
+      </footer>
     </div>
   );
 }
--- client/src/Main.js
+++ client/src/Main.js
@@ -4,36 +4,26 @@ import { TableContainer } from './TableContainer';
 import { SearchParametersContainer } from './SearchParametersContainer';
 
 export class Main extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {};
+    this.state = {};
 
-        this.updateTable = this.updateTable.bind(this);
-    }
+    this.updateTable = this.updateTable.bind(this);
+  }
 
-    updateTable(newData) {
-        this.setState({
-            tableData: newData
-        });
-    }
+  updateTable(newData) {
+    this.setState({
+      tableData: newData
+    });
+  }
 
-    render() {
-        return (
-            <div>
-                <div className="content">
-                    <header>
-                        <h1>Pathfinder 1E Spell Search</h1>
-                    </header>
-                    <br />
-                    <SearchParametersContainer onUpdate={this.updateTable} />
-                    <TableContainer tableData={this.state.tableData} />
-                    <div className="push"></div>
-                </div>
-                <footer>
-                    <h6>Designed by Brendon Hudnell. 2020</h6>
-                </footer>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="main">
+        <SearchParametersContainer onUpdate={this.updateTable} />
+        <TableContainer tableData={this.state.tableData} />
+      </div>
+    );
+  }
 }
